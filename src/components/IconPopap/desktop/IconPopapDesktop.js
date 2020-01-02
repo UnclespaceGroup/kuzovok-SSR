0d27e4f6a99fe71b45ad6e4f121beb0ccf7b047d
Ivan Dmitrachkov <dmitrachkovivan@gmail.com>
2020-01-02 19:24:43 +0300

    fix

--- src/components/IconPopap/desktop/IconPopapDesktop.js
+++ src/components/IconPopap/desktop/IconPopapDesktop.js
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import css from './IconPopap.module.scss'
-import { MdMessage } from 'react-icons/md'
+import cn from 'classnames'
+import { MdMessage, MdPhone, MdClose } from 'react-icons/md'
+import { FaVk } from 'react-icons/fa'
+import { EMAIL, MAIN_PHONE, VK_ADDRESS } from 'constants/contacts'
 
 const IconPopapDesktop = ({ className }) => {
-  // const [ isOpen, setIsOpen ] = useState()
+  const [ isOpen, setIsOpen ] = useState()
   return (
-    <div className={className}>
-      <div className={css.icon}>
-        <MdMessage />
+    <div className={cn(className)}>
+      <div className={css.wrapper}>
+        {
+          isOpen && <div className={css.modal}>
+            <div className={css.item}><MdMessage />{EMAIL}</div>
+            <div className={css.item}><MdPhone />{MAIN_PHONE}</div>
+            <div className={css.item}><FaVk />{VK_ADDRESS}</div>
+            <div className={css.close} onClick={() => setIsOpen(false)}><MdClose /></div>
+          </div>
+        }
+        <div className={css.icon} onClick={() => setIsOpen(!isOpen)}>
+          <MdMessage />
+        </div>
       </div>
     </div>
   )
